7083725eeed1e5b4148914557c6e5ab191f347e8
Deepak Lokare <deepaklokare.24@gmail.com>
2024-12-26 23:39:22 -0600

    updated vercel domain url

--- api.py
+++ api.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://travelagent-production.up.railway.app"],  # Update with your frontend's URL
+    allow_origins=["http://localhost:3000", "https://travel-planner-one-virid.vercel.app"],  # Update with your frontend's URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
